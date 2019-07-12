d1b0d94c17b3dfbf0be392fa0f2a19e505041713
sdf <theo.farnole@gmail.com>
2019-07-13 00:04:44 +0200

    Display level name on screen, to check if Level Selection works

---
+++ Assets/Scripts/Game/Managers/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textSceneName;
+
+    void Awake()
+    {
+        _textSceneName.text = SceneManager.GetActiveScene().name;
+    }
+}
