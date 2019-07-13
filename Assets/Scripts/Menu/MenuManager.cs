f7864cbff08136e1d90294cd45b0eeebf3bdecbe
sdf <theo.farnole@gmail.com>
2019-07-13 02:39:45 +0200

    Improve code

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -12,9 +12,6 @@ public class MenuManager : MonoBehaviour
     public readonly static float CHANGE_LEVEL_TIME_DELAY = 0.3f;
 
     #region Fields
-    [Header("Players' Avatar")]
-    [SerializeField] private GameObject[] _playersAvatar = new GameObject[4];
-
     private bool _canChangeLevel = true;
     #endregion
 
@@ -24,18 +21,6 @@ public class MenuManager : MonoBehaviour
         AirConsole.instance.onMessage += OnMessage;
         AirConsole.instance.onConnect += OnConnect;
         AirConsole.instance.onDisconnect += OnDisconnect;
-    }
-
-    void Start()
-    {
-        // hide avatar until player join
-        for (int i = 0; i < _playersAvatar.Length; i++)
-        {
-            _playersAvatar[i].transform.ActionForEachChildren((GameObject c) =>
-            {
-                c.SetActive(false);
-            });
-        }
     }
 
     void OnDestroy()
@@ -69,38 +54,15 @@ public class MenuManager : MonoBehaviour
         }
     }
 
-    // TODO: Optimize this method!
+    
     void OnConnect(int device_id)
     {
-        Debug.Log("On connect!");
-
         var playerAvatarIndex = AirConsole.instance.GetControllerDeviceIds().Count - 1;
-
-        Debug.Log("playerAvatarIndex " + playerAvatarIndex);
-
-        _playersAvatar[playerAvatarIndex].transform.ActionForEachChildren((GameObject c) =>
-        {
-            c.SetActive(true);
-
-            var image = c.GetComponent<Image>();
-
-            if (image)
-            {
-                image.color = ((CharID)playerAvatarIndex).ToColor();
-            }
-
-            var text = c.GetComponent<TextMeshProUGUI>();
-
-            if (text)
-            {
-                text.text = AirConsole.instance.GetNickname(device_id);
-            }
-        });
+        UIMenuManager.Instance.DisplayPlayer(playerAvatarIndex, device_id);
     }
 
     void OnDisconnect(int device_id)
     {
         Debug.LogError("OnDisconnect not implemented");
     }
-
 }
---
+++ Assets/Scripts/Menu/UIMenuManager.cs
@@ -0,0 +1,54 @@
+﻿using NDream.AirConsole;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenuManager : Singleton<UIMenuManager>
+{
+    #region Fields
+    [SerializeField] private GameObject[] _playersAvatar = new GameObject[4];
+    [Space]
+    [SerializeField] private TextMeshProUGUI _textWaitingForPlayers;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Start()
+    {
+        // hide avatar until player join
+        for (int i = 0; i < _playersAvatar.Length; i++)
+        {
+            _playersAvatar[i].transform.ActionForEachChildren((GameObject c) =>
+            {
+                c.SetActive(false);
+            });
+        }
+    }
+    #endregion
+
+    // TODO: Optimize this method!
+    public void DisplayPlayer(int index, int device_id)
+    {
+        _textWaitingForPlayers.gameObject.SetActive(false);
+
+        _playersAvatar[index].transform.ActionForEachChildren((GameObject c) =>
+        {
+            c.SetActive(true);
+
+            var image = c.GetComponent<Image>();
+
+            if (image)
+            {
+                image.color = ((CharID)index).ToColor();
+            }
+
+            var text = c.GetComponent<TextMeshProUGUI>();
+
+            if (text)
+            {
+                text.text = AirConsole.instance.GetNickname(device_id);
+            }
+        });
+    }
+}
