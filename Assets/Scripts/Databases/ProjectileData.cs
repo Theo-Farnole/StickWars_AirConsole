1212adf29886a05fd9e32fdb42a6863c533ca89b
sdf <theo.farnole@gmail.com>
2019-07-15 01:20:45 +0200

    [+] projectile prefab, linking on CharController variable

---
+++ Assets/Scripts/Databases/ProjectileData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TO Databases/Projectile")]
+public class ProjectileData : MonoBehaviour
+{
+    [SerializeField] private int _speed = 3;
+
+    public int Speed { get => _speed; }
+}
---
+++ Assets/Scripts/Game/Character/Projectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class Projectile : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] private ProjectileData _data;
+
+    [HideInInspector] public int damage = 10;
+    [HideInInspector] public Entity sender;
+
+    private Vector3 _direction = Vector3.right;
+    #endregion
+
+    #region Properties
+    public Vector3 Direction
+    {
+        get
+        {
+            return _direction;
+        }
+
+        set
+        {
+            _direction = value.normalized;
+        }
+    }
+    #endregion
+
+    void Update()
+    {
+        transform.position += _direction * _data.Speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var entity = collision.GetComponent<Entity>();
+
+        if (entity != null && entity != sender)
+        {
+            entity.GetDamage(damage, sender);
+            Destroy(gameObject);
+        }
+    }
+}
