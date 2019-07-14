374a45ce0fb6e8466adb7a8a18249c1051b21119
sdf <theo.farnole@gmail.com>
2019-07-14 16:20:10 +0200

    [+] UIManager & UISystem can display Gamemode data (kills, etc...)

--- Assets/Scripts/Game/Managers/UIManager.cs
+++ Assets/Scripts/Game/Managers/UIManager.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _textSceneName;
+    [Space]
+    [SerializeField] private GameObject[] _gamemodeData = new GameObject[GameManager.MAX_PLAYERS];
 
     void Awake()
     {
         _textSceneName.text = SceneManager.GetActiveScene().name;
+    }
+
+    public void UpdateGamemodeData(string[] arrayStr)
+    {
+        for (int i = 0; i < arrayStr.Length && i < GameManager.MAX_PLAYERS; i++)
+        {
+            _gamemodeData[i].GetComponentInChildren<TextMeshProUGUI>().text = arrayStr[i];
+            _gamemodeData[i].GetComponentInChildren<Image>().color = ((CharID)i).ToColor();
+        }
     }
 }
