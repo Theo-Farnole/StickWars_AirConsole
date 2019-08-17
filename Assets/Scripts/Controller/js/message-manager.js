7f51d126bd0daac59719be7d551e7badb8b0439f
Theo <theo.farnole@gmail.com>
2019-08-17 17:54:27 +0200

    improve input system

--- Assets/Scripts/Controller/js/message-manager.js
+++ Assets/Scripts/Controller/js/message-manager.js
@@ -3,56 +3,70 @@ var airconsole;
 function init() {
     airconsole = new AirConsole({
         "orientation": "landscape"
-    }); 
+    });
 
     ViewManager.init();
     ViewManager.show("Play");
 
-    airconsole.onMessage = function(from, data) 
-    {
-        if (from == AirConsole.SCREEN)
-        {
-            if (data.view)
-            {
+    airconsole.onMessage = function (from, data) {
+        if (from == AirConsole.SCREEN) {
+            if (data.view) {
                 ViewManager.show(data.view);
             }
-            
-            if (data.bgColor)
-            {
+
+            if (data.bgColor) {
                 document.body.style.backgroundColor = data.bgColor;
-            } 
+            }
         }
     };
 
-    // to avoid play on awake
-    // replace "horizontal(-1)" by
-    // function () { horizontal(-1)}
-    addButton("button_left", horizontal(-1), horizontal(0));
-    addButton("button_right", horizontal(1), horizontal(0));
-    addButton("button_b", bPressed(true), bPressed(false));
-    addButton("button_x", xPressed(true), xPressed(false));
-    addButton("button_a", aPressed(true), aPressed(false));
+    addButton("button_left",
+        function () {
+            horizontal(-1)
+        },
+        function () {
+            horizontal(0)
+        });
+    addButton("button_right",
+        function () {
+            horizontal(1)
+        },
+        function () {
+            horizontal(0)
+        });
+    addButton("button_b",
+        function () {
+            bPressed(true)
+        },
+        function () {
+            bPressed(false)
+        });
+    addButton("button_x",
+        function () {
+            xPressed(true)
+        },
+        function () {
+            xPressed(false)
+        });
+    addButton("button_a",
+        function () {
+            aPressed(true)
+        },
+        function () {
+            aPressed(false)
+        });
 }
 
 
-function addButton(id, activeFunction, disableFunction)
-{
-    setEventHandler(id, "ontouchstart", activeFunction);
-    setEventHandler(id, "ontouchend", disableFunction);
+function addButton(id, activeFunction, disableFunction) {
 
-    setEventHandler(id, "onmousedown", activeFunction);
-    setEventHandler(id, "onmouseup", disableFunction);
-}
+    var obj = document.getElementById(id);
 
-function setEventHandler(obj, name, fn) {
-    if (typeof obj == "string") {
-        obj = document.getElementById(obj);
-    }
-    if (obj.addEventListener) {
-        return(obj.addEventListener(name, fn));
-    } else if (obj.attachEvent) {
-        return(obj.attachEvent("on" + name, function() {return(fn.call(obj));}));
-    }
+    obj.addEventListener("touchstart", activeFunction);
+    obj.addEventListener("touchend", disableFunction);
+
+    obj.addEventListener("mousedown", activeFunction);
+    obj.addEventListener("mouseup", disableFunction);
 }
 
 function horizontal(amount) {
