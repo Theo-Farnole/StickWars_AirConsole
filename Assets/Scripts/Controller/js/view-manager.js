1059d06db6aba99bd52ea1b27580ff743df802f5
Theo <theo.farnole@gmail.com>
2019-08-17 17:36:26 +0200

    beautify view manager

--- Assets/Scripts/Controller/js/view-manager.js
+++ Assets/Scripts/Controller/js/view-manager.js
@@ -4,31 +4,31 @@ var ViewManager = {
     views: {},
     current_view_id: null,
 
-    init: function() {
+    init: function () {
         var views = document.querySelectorAll('.view');
         for (var i = 0; i < views.length; i++) {
-        this.views[views[i].id] = views[i];
-        }
-        return this;
-    }, 
-
-    show: function(id) {
-        console.log("Change view with " + id);
-
-        var view = this.views[id];
-        if (view) {
-        this.current_view_id = id;
-        this.hideAll();
-        view.style.display = "block";
-        } else {
-        console.warn("Could not find view with ID:", id);
+            this.views[views[i].id] = views[i];
         }
         return this;
     },
 
-    hideAll: function() {
+    show: function (id) {
+        console.log("Change view with " + id);
+
+        var view = this.views[id];
+        if (view) {
+            this.current_view_id = id;
+            this.hideAll();
+            view.style.display = "block";
+        } else {
+            console.warn("Could not find view with ID:", id);
+        }
+        return this;
+    },
+
+    hideAll: function () {
         for (var key in this.views) {
-        this.views[key].style.display = "none";
+            this.views[key].style.display = "none";
         }
     }
 };
