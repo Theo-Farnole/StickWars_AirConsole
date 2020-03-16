fb626fe458674f807e9dfca2eaca538c2e9e406f
Theo <theo.farnole@gmail.com>
2020-03-16 11:44:39 +0100

    Add: Abstract TextBox Trigger

---
+++ Assets/Scripts/Game/Tutorial Texts Boxes/AbstractTextBoxTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TutorialTextBoxManager))]
+public abstract class AbstractTextBoxTrigger : MonoBehaviour
+{
+    [SerializeField] private string _textBoxContent;
+    
+    protected void TriggerTextBox()
+    {
+        var tutorialTextBoxManager = GetComponent<TutorialTextBoxManager>();
+        tutorialTextBoxManager.EnqueueTutorialTextBox(_textBoxContent);
+    }
+
+    protected void StartTimer(float duration, Action task)
+    {
+        this.ExecuteAfterTime(duration, task);
+    }
+}
