c484626498890a7f3fb18cc6bc30a2ea69130789
Theo <theo.farnole@gmail.com>
2020-03-16 10:57:38 +0100

    Add: Tutorial TextBox Manager

---
+++ Assets/Scripts/Game/Miscellaneous/TutorialTextBoxManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialTextBoxManager : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] private float _displayDuration;
+    [SerializeField] private TextMeshProUGUI _textBox;
+
+    private Queue<string> _queuedTextsBoxes = new Queue<string>();
+    private float _timerTextBoxDisplay = 0;
+
+    private string _currentTextBoxContent = string.Empty;
+    #endregion
+
+    #region Properties
+    public bool IsMessageDisplaying => !string.IsNullOrEmpty(_currentTextBoxContent);
+    #endregion
+
+    #region Methods
+    #region MonoBehaviour Callbacks
+    void Start()
+    {
+        _textBox.enabled = false;    
+    }
+
+    void Update()
+    {
+        ManageTextBoxDisplay();
+    }
+    #endregion
+
+    #region Public methods
+    public void EnqueueTutorialTextBox(string message)
+    {
+        // don't have to wait to display message
+        if (_queuedTextsBoxes.Count == 0 && !IsMessageDisplaying)
+        {
+            DisplayTextBox(message);
+        }
+        else
+        {
+            // otherwise, add message to queue
+            _queuedTextsBoxes.Enqueue(message);
+        }
+    }
+    #endregion
+
+    #region Methods managing messages display
+    void ManageTextBoxDisplay()
+    {
+        // don't manage text box, if no one is displaying
+        if (!IsMessageDisplaying)
+            return;
+
+        _timerTextBoxDisplay += Time.deltaTime;
+
+        // is timer over ?
+        if (_timerTextBoxDisplay >= _displayDuration)
+        {
+            _timerTextBoxDisplay = 0;
+            DisplayNextTextBox();
+        }
+    }
+
+    void DisplayNextTextBox()
+    {
+        // is a message is waiting to be displayed ?
+        if (_queuedTextsBoxes.Count > 0)
+        {
+            var nextMessage = _queuedTextsBoxes.Dequeue();
+            DisplayTextBox(nextMessage);
+        }
+        else
+        {
+            // disable text component
+            if (_textBox.enabled)
+                _textBox.enabled = false;
+
+            _currentTextBoxContent = string.Empty;
+        }
+    }
+
+    void DisplayTextBox(string message)
+    {
+        if (!_textBox.enabled)
+            _textBox.enabled = true;
+
+        _currentTextBoxContent = message;
+        _textBox.text = _currentTextBoxContent;
+    }
+    #endregion
+    #endregion
+}
