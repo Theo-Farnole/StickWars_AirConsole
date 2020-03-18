2899e084afd4df7a66c39c02f20b96f4dc724fe3
Theo <theo.farnole@gmail.com>
2020-03-18 11:06:30 +0100

    Remove: AbstractCharState TODO comments

--- Assets/Scripts/Game/Character/States/AbstractCharState.cs
+++ Assets/Scripts/Game/Character/States/AbstractCharState.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public abstract class AbstractCharState : OwnerState<CharController>
 {
-    // TODO: Inherit others CharState from this class
-
-    // TODO: OnStateChanged events in CharController
     public AbstractCharState(CharController owner) : base(owner)
     { }
 }
