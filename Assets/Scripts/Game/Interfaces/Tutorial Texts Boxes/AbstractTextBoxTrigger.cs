449966c1e79a92fb3d0b76ed2951c210a55604e7
Theo <theo.farnole@gmail.com>
2020-03-18 09:56:28 +0100

    Add: [optimization] Auto Destruction behavior in TextBox Trigger

--- Assets/Scripts/Game/Interfaces/Tutorial Texts Boxes/AbstractTextBoxTrigger.cs
+++ Assets/Scripts/Game/Interfaces/Tutorial Texts Boxes/AbstractTextBoxTrigger.cs
@@ -7,11 +7,24 @@ using UnityEngine;
 public abstract class AbstractTextBoxTrigger : MonoBehaviour
 {
     [SerializeField] private string _textBoxContent;
-    
+    [Header("Auto Destruction")]
+    [SerializeField] private bool _destroyAfterTriggers = false;
+    [SerializeField] private int _triggersCountToDestroy = 2;
+
+    private int _currentTriggerCount = 0;
+
     protected void TriggerTextBox()
     {
         var tutorialTextBoxManager = GetComponent<TutorialTextBoxManager>();
         tutorialTextBoxManager.EnqueueTutorialTextBox(_textBoxContent);
+
+        if (_destroyAfterTriggers)
+        {
+            _currentTriggerCount++;
+
+            if (_currentTriggerCount >= _triggersCountToDestroy)            
+                Destroy(this); // only destroy this component            
+        }
     }
 
     protected void StartTimer(float duration, Action task)
