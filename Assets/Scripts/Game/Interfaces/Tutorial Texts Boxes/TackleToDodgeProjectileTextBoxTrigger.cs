009df10f36ac4d24001652d08556e270a8dd7f24
Theo <theo.farnole@gmail.com>
2020-03-18 09:56:54 +0100

    Add: [text box] tackle to dodge projectile

---
+++ Assets/Scripts/Game/Interfaces/Tutorial Texts Boxes/TackleToDodgeProjectileTextBoxTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TackleToDodgeProjectileTextBoxTrigger : AbstractTextBoxTrigger
+{
+    [Header("Condition")]
+    [SerializeField] private int _projectilesHitCountToTrigger = 8;
+
+    private Dictionary<CharacterEntity, int> _projectilesHitCount = new Dictionary<CharacterEntity, int>();
+
+    void OnEnable()
+    {
+        Projectile.OnProjectileHitEntity += OnProjectileHitEntity;
+    }
+
+    void OnDisable()
+    {
+        Projectile.OnProjectileHitEntity -= OnProjectileHitEntity;
+    }
+
+    void OnProjectileHitEntity(Entity ent)
+    {
+        if (!(ent is CharacterEntity))
+            return;
+
+        var key = ent as CharacterEntity;
+
+        // create new entry in dictionary
+        if (!_projectilesHitCount.ContainsKey(key))
+            _projectilesHitCount.Add(key, 0);
+
+        // increase hit count
+        _projectilesHitCount[key]++;
+        
+        if (_projectilesHitCount[key] == _projectilesHitCountToTrigger)
+        {
+            TriggerTextBox();
+        }
+    }
+}
