ca181f8534311d22bbe09d643d5f83811c4650cd
Theo <theo.farnole@gmail.com>
2020-03-19 15:31:29 +0100

    Refactor: [level_data] Split RespawnArea into LineData separated file

--- Assets/Scripts/Game/Levels/LevelData.cs
+++ Assets/Scripts/Game/Levels/LevelData.cs
@@ -5,26 +5,12 @@ using UnityEngine;
 
 public class LevelData : Singleton<LevelData>
 {
-    #region Classes
-    [System.Serializable]
-    public class RespawnArea
-    {
-        public Vector3 p1 = Vector3.left;
-        public Vector3 p2 = Vector3.right;
-
-        public void DrawGizmos()
-        {
-            Gizmos.DrawLine(p1, p2);
-        }
-    }
-    #endregion
-
     #region Fields
     [SerializeField] private Transform[] _virusSpawnerPosition = new Transform[3];
     [Space]
     [EnumNamedArray(typeof(CharId))]
     [SerializeField] private Transform[] _defaultSpawnPoint = new Transform[4];
-    [SerializeField] private RespawnArea[] _respawnArea;
+    [SerializeField] private LineData[] _respawnArea;
     #endregion
 
     #region Properties
@@ -32,6 +18,18 @@ public class LevelData : Singleton<LevelData>
     #endregion
 
     #region Methods
+    #region MonoBehaviour Callbacks
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < _respawnArea.Length; i++)
+        {
+            _respawnArea[i].DrawGizmos();
+        }
+    }
+    #endregion
+
+    #region Getter
+    #region Spawnpoints
     public Vector3 GetDefaultSpawnPoint(CharId charId)
     {
         return _defaultSpawnPoint[(int)charId].position;
@@ -39,16 +37,11 @@ public class LevelData : Singleton<LevelData>
 
     public Vector3 GetRandomSpawnPoint()
     {
-        int random = Random.Range(0, _respawnArea.Length);
-
-        Vector3 p = Vector3.zero;
-        p.x = Random.Range(_respawnArea[random].p1.x, _respawnArea[random].p2.x);
-        p.y = Random.Range(_respawnArea[random].p1.y, _respawnArea[random].p2.y);
-        p.z = Random.Range(_respawnArea[random].p1.y, _respawnArea[random].p2.z);
-
-        return p;        
+        return _respawnArea.GetRandomPoint();        
     }
+    #endregion
 
+    #region VirusSpawner position
     public Vector3 GetRandomVirusSpawnerPosition()
     {
         int randomIndex = Random.Range(0, _virusSpawnerPosition.Length);
@@ -64,13 +57,6 @@ public class LevelData : Singleton<LevelData>
         int randomIndex = Random.Range(0, result.Length);
         return result[randomIndex].position;
     }
-
-    void OnDrawGizmos()
-    {
-        for (int i = 0; i < _respawnArea.Length; i++)
-        {
-            _respawnArea[i].DrawGizmos();
-        }
-    }
+    #endregion
     #endregion
 }
---
+++ Assets/Scripts/Game/Levels/LineData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LineData
+{
+    public Vector3 p1 = Vector3.left;
+    public Vector3 p2 = Vector3.right;
+
+    public void DrawGizmos()
+    {
+        Gizmos.DrawLine(p1, p2);
+    }
+
+    public Vector3 GetRandomPointInsideLine()
+    {
+        Vector3 point = Vector3.zero;
+
+        point.x = Random.Range(p1.x, p2.x);
+        point.y = Random.Range(p1.y, p2.y);
+        point.z = Random.Range(p1.y, p2.z);
+
+        return point;
+    }
+}
+
+public static class LineArrayExtension
+{
+    /// <summary>
+    /// Select a random element in array, GetRandomPointInsideLine() and return it
+    /// </summary>
+    public static Vector3 GetRandomPoint(this LineData[] lineArray)
+    {
+        int randomIndex = Random.Range(0, lineArray.Length);
+        return lineArray[randomIndex].GetRandomPointInsideLine();
+    }
+}
