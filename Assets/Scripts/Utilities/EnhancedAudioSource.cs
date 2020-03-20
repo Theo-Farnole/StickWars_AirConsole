10f4b6f6b649e1ecc7d7fae3eef4cc48ddab5809
Theo <theo.farnole@gmail.com>
2020-03-20 15:50:27 +0100

    Add: EnhancedAudioSource to set to root then play

---
+++ Assets/Scripts/Utilities/EnhancedAudioSource.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EnhancedAudioSource : MonoBehaviour
+{
+    public void SetToRootThenPlay()
+    {
+        transform.parent = null;
+        GetComponent<AudioSource>().Play();
+    }
+}
