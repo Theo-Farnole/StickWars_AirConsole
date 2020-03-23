3cabbfdd29b898b112097a5674a7815668f87459
Theo <theo.farnole@gmail.com>
2020-03-23 17:28:52 +0100

    Add: [cursor] CursorManager that follow the real mouse

---
+++ Assets/Scripts/Game/Levels/Level Layout/Cursor/CursorManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3;
+
+    void Update()
+    {
+        InGameCursorFollowRealCursor();
+    }
+
+    private void InGameCursorFollowRealCursor()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        MoveTo(mousePosition);
+    }
+
+    void MoveTo(Vector3 targetPosition)
+    {
+        // security: lock cursor position
+        targetPosition.z = transform.position.z;
+
+        Vector3 direction = targetPosition - transform.position;
+        transform.position += direction * (_speed * Time.deltaTime);
+    }
+}
