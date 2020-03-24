93f7a756e86dd04896e6c5b34ab48c17752898ed
Theo <theo.farnole@gmail.com>
2020-03-24 09:48:55 +0100

    Add: [cursor] cursor's sprite that change in function of a raycast

--- Assets/Scripts/Game/Levels/Level Layout/Cursor/CursorManager.cs
+++ Assets/Scripts/Game/Levels/Level Layout/Cursor/CursorManager.cs
@@ -2,20 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3;
+    #region Enums
+    public enum CursorState
+    {
+        Default,
+        Hover,
+        Drag
+    }
+    #endregion
 
+    #region Fields
+    [SerializeField] private float _speed = 3;
+    [SerializeField, EnumNamedArray(typeof(CursorState))] private Sprite[] _spritesCursor = new Sprite[3];
+
+    // cache variable
+    private SpriteRenderer _spriteRenderer;
+    #endregion
+
+    #region Properties
+    public SpriteRenderer SpriteRenderer {
+        get
+        {
+            if (_spriteRenderer == null)
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+
+            return _spriteRenderer;
+        }
+    }
+    #endregion
+
+    #region Methods
+    #region MonoBehaviour Callbacks
     void Update()
     {
         InGameCursorFollowRealCursor();
+        UpdateCursorSprite();
     }
+    #endregion
 
     private void InGameCursorFollowRealCursor()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         MoveTo(mousePosition);
+    }
+
+    void UpdateCursorSprite()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.forward);
+
+        // If hits something
+        if (hit.collider != null)
+        {
+            SetSprite(CursorState.Hover);
+        }
+        else
+        {
+            SetSprite(CursorState.Default);
+        }
+    }
+
+    void SetSprite(CursorState cursorState)
+    {
+        int index = (int)cursorState;
+        Sprite sprite = _spritesCursor[index];
+
+        SpriteRenderer.sprite = sprite;
     }
 
     void MoveTo(Vector3 targetPosition)
@@ -26,4 +81,5 @@ public class CursorManager : MonoBehaviour
         Vector3 direction = targetPosition - transform.position;
         transform.position += direction * (_speed * Time.deltaTime);
     }
+    #endregion
 }
