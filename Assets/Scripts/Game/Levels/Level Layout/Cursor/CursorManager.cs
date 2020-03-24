476219bcd23f2cd3577c4f3291cf48e7e96f846e
Theo <theo.farnole@gmail.com>
2020-03-24 10:50:12 +0100

    Add: [cursor] LevelLayout elements dragging

--- Assets/Scripts/Game/Levels/Level Layout/Cursor/CursorManager.cs
+++ Assets/Scripts/Game/Levels/Level Layout/Cursor/CursorManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: update _draggedGameObject position in Update
+
 [RequireComponent(typeof(SpriteRenderer))]
 public class CursorManager : MonoBehaviour
 {
@@ -18,12 +20,24 @@ public class CursorManager : MonoBehaviour
     [SerializeField] private float _speed = 3;
     [SerializeField, EnumNamedArray(typeof(CursorState))] private Sprite[] _spritesCursor = new Sprite[3];
 
+    // move to variable
+    private bool _isMoving = false;
+    private Vector3 _directionToMoveToPosition;
+    private Vector3 _moveToPosition;
+
+    // drag variables
+    private LevelLayoutElement _draggedElement;
+    private Vector3 _deltaPositionDraggedElement;
+
     // cache variable
     private SpriteRenderer _spriteRenderer;
     #endregion
 
     #region Properties
-    public SpriteRenderer SpriteRenderer {
+    public RaycastHit2D HitBehindCursor { get => Physics2D.Raycast(transform.position, Vector3.forward); }
+
+    public SpriteRenderer SpriteRenderer
+    {
         get
         {
             if (_spriteRenderer == null)
@@ -38,24 +52,66 @@ public class CursorManager : MonoBehaviour
     #region MonoBehaviour Callbacks
     void Update()
     {
-        InGameCursorFollowRealCursor();
+        // inputs
+        Input_FollowRealCursor();
+        Input_Drag();
+
+        // movements
+        ManageMovement();
+    }
+
+    void LateUpdate()
+    {
         UpdateCursorSprite();
     }
     #endregion
 
-    private void InGameCursorFollowRealCursor()
+    #region Movements methods
+    void Input_FollowRealCursor()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         MoveTo(mousePosition);
     }
 
+    void ManageMovement()
+    {
+        if (_isMoving)
+        {
+            Vector3 delta = _directionToMoveToPosition * (_speed * Time.deltaTime);
+
+            transform.position += delta;
+            DragGameObject(delta);
+        }
+    }
+
+    public void MoveTo(Vector3 targetPosition)
+    {
+        // security: lock cursor position
+        targetPosition.z = transform.position.z;
+
+        _directionToMoveToPosition = (targetPosition - transform.position).normalized;
+        _moveToPosition = targetPosition;
+
+        StartMovement();
+    }
+
+    public void StartMovement()
+    {
+        _isMoving = true;
+    }
+
+    public void StopMovement()
+    {
+        _isMoving = false;
+    }
+    #endregion
+
+    #region Aspects methods
     void UpdateCursorSprite()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.forward);
-
         // If hits something
-        if (hit.collider != null)
+        if (HitBehindCursor.collider != null)
         {
             SetSprite(CursorState.Hover);
         }
@@ -72,14 +128,51 @@ public class CursorManager : MonoBehaviour
 
         SpriteRenderer.sprite = sprite;
     }
+    #endregion
 
-    void MoveTo(Vector3 targetPosition)
+    #region Dragging methods
+    void Input_Drag()
     {
-        // security: lock cursor position
-        targetPosition.z = transform.position.z;
+        var cachedHit = HitBehindCursor;
 
-        Vector3 direction = targetPosition - transform.position;
-        transform.position += direction * (_speed * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0) && cachedHit.collider != null)
+        {
+            StopDrag();
+
+            StartDrag(cachedHit.collider.gameObject.GetComponentInParent<LevelLayoutElement>());
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            StopDrag();
+        }
     }
+
+    void DragGameObject(Vector3 offset)
+    {
+        if (_draggedElement == null)
+            return;
+
+        _draggedElement.transform.position += offset;
+    }
+
+    public void StartDrag(LevelLayoutElement levelLayoutElement)
+    {
+        if (levelLayoutElement == null)
+        {
+            Debug.LogWarningFormat("{0} start drag of null element! Please use StopDrag() if you want to stop dragging.", name);
+            return;
+        }
+
+        _draggedElement = levelLayoutElement;
+
+        _deltaPositionDraggedElement = transform.position - levelLayoutElement.transform.position;
+    }
+
+    public void StopDrag()
+    {
+        _draggedElement = null;
+    }
+    #endregion
     #endregion
 }
