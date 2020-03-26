8eee6cef2b71ed084a8dbcb7ebb7aa929512e54f
Theo <theo.farnole@gmail.com>
2020-03-26 11:11:48 +0100

    Add: [utilities] MultipleAudioSources script to play a random AudioSource from an array

---
+++ Assets/Scripts/Utilities/MultipleAudioSources.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleAudioSources : MonoBehaviour
+{
+    [SerializeField] private AudioSource[] _audioSources;
+
+    public void PlayRandomAudioSource()
+    {
+        int index = Random.Range(0, _audioSources.Length);
+        _audioSources[index].Play();
+    }
+}
