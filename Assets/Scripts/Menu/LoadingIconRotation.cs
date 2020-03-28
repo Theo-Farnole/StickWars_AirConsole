e5dbe06bfb579ed38f62eab467acb7f366ff8503
Theo <theo.farnole@gmail.com>
2020-03-28 12:11:01 +0100

    Change: [interface menu] improve loading panel (add a rotation icon)

---
+++ Assets/Scripts/Menu/LoadingIconRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingIconRotation : MonoBehaviour
+{
+    [SerializeField] private int _segmentsCount = 12;
+    [SerializeField] private float _timePerSegment = 0.1f;
+    [SerializeField] private bool _rotateClockwise = true;
+
+    private float _timer = 0;
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= _timePerSegment)
+        {
+            _timer = 0;
+            RotateOneSegments();
+        }
+    }
+
+    void RotateOneSegments()
+    {
+        float angleForOneSegment = 360 / _segmentsCount;
+
+        if (_rotateClockwise)
+            angleForOneSegment *= -1; // reverse angle delta
+
+        var eulerAngles = transform.eulerAngles;        
+        eulerAngles.z += angleForOneSegment;
+        transform.eulerAngles = eulerAngles;
+    }
+}
