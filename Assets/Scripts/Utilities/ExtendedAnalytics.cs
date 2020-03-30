0b85cb91e2671f0937a99738a0ff85a0abb68dbf
Theo <theo.farnole@gmail.com>
2020-03-30 11:46:50 +0200

    Add: [analytics] Extended Analytics class

---
+++ Assets/Scripts/Utilities/ExtendedAnalytics.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public static class ExtendedAnalytics
+{
+    public static void SendEvent(string eventName, IDictionary<string, object> eventData = null)
+    {
+        var result = AnalyticsEvent.Custom(eventName, eventData);
+
+#if UNITY_EDITOR
+        Debug.LogFormat("Analytics # Event {0} fired with result {1}. (param: {2})", eventName, result, eventData);
+#endif
+    }
+}
