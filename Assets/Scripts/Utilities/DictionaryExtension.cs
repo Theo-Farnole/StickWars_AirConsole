3abee9d035c9bf9974ecf695583edae26f62e282
Theo <theo.farnole@gmail.com>
2020-03-30 12:08:32 +0200

    Change: [extended analytics] fix event's parameters to string in debug log

---
+++ Assets/Scripts/Utilities/DictionaryExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class DictionaryExtension
+{
+    public static string ToDebugString<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+    {
+        return "{\n" + string.Join(",\n", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
+    }
+}
